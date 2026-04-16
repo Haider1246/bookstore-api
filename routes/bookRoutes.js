691f051f0804d5eb1990a30a8ec3d03840b75f22
Haider1246 <mr.haider346@gmail.com>
2026-04-16 17:20:30 +0500

    Update bookRoutes.js

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -2,14 +2,13 @@ const express = require('express');
 const router = express.Router();
 const bookController = require('../controllers/bookController');
 
-// Raste (Endpoints)
-router.post('/', bookController.createBook); // POST /books
-router.get('/', bookController.getAllBooks); // GET /books
+//Endpoints
+router.post('/', bookController.createBook); // POST
+router.get('/', bookController.getAllBooks); // GET
 
 module.exports = router;
-// Pehle wale routes ke niche ye add karein:
 
-router.get('/:id', bookController.getBookById);   // GET /books/ID
-router.put('/:id', bookController.updateBook);   // PUT /books/ID
-router.delete('/:id', bookController.deleteBook); // DELETE /books/ID
+router.get('/:id', bookController.getBookById);   // GET
+router.put('/:id', bookController.updateBook);   // PUT
+router.delete('/:id', bookController.deleteBook); // DELETE
 
